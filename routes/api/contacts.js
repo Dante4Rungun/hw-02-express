2f005060c13e4c442aee58f73b4dab046e365b70
Dante4Rungun <denischuprin9619@gmail.com>
2022-11-17 15:50:26 +0200

    hw02 version

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,41 @@
 const express = require('express')
+const methods = require('../../models/contacts')
+const validation = require('../../validation/validation')
+const {
+  validate
+} = require('../../validation/validationMiddleware')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const response =  await methods.listContacts()
+  res.status(response.status).json(response.data)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const response = await methods.getContactById(contactId)
+  res.status(response.status).json(response.data)
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validate(validation.contact), async (req, res, next) => {
+  const { name, email, phone } = req.body
+  console.log(name)
+  const response = await methods.addContact(name, email, phone)
+  res.status(response.status).json(response.data)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const response = await methods.removeContact(contactId)
+  res.status(response.status).json(response.data)
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', validate(validation.contact), async (req, res, next) => {
+  const { contactId } = req.params
+  const { name, email, phone } = req.body
+  const response = await methods.updateContact(contactId, name, email, phone)
+  res.status(response.status).json(response.data)
 })
 
 module.exports = router
