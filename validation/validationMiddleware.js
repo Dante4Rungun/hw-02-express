4a9c27c902910a065898dc63c9484e643e2c8717
Dante4Rungun <denischuprin9619@gmail.com>
2022-11-20 13:27:32 +0200

    Try git bush

--- validation/validationMiddleware.js
+++ validation/validationMiddleware.js
@@ -7,16 +7,16 @@ exports.validate = (schema) => (req, res, next) => {
     )
   }
   else {
-    const {
-      error
-    } = schema.validate(req.body);
-    if (error) {
-      res.status(400)
-        .send(
-          utils.createResponse("400",error.details[0].message,true) 
-        );
-    } else {
-      next();
+  const {
+    error
+  } = schema.validate(req.body);
+  if (error) {
+    res.status(400)
+      .send(
+        utils.createResponse("400",error.details[0].message,true) 
+      );
+  } else {
+    next();
     }
   }
 };
