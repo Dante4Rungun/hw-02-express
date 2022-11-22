203cc510149d7e82db30c2c050bb4bfe2bc466a3
Dante4Rungun <denischuprin9619@gmail.com>
2022-11-22 20:52:06 +0200

    findOneAnd...

--- controllers/contacts/remove.js
+++ controllers/contacts/remove.js
@@ -2,7 +2,7 @@ const Contact = require('../../models/contact')
 
 const remove = async (req, res) => {
     const { contactId } = req.params
-    await Contact.remove({ _id: contactId })
+    await Contact.findOneAndRemove({ _id: contactId })
         .then(obj => {
             res.status(200).json({"message": "contact deleted"})       
         })
